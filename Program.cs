84038787429bcd6026084d653e528b29fb90c264
Ajisebutu Kayode <ajisebutukayode@gmail.com>
2026-08-05 00:40:21 +0100

    my first console app

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Console.WriteLine("Hello, Wolrd");
+// Console.WriteLine("My name is Kayode");
+
+//varriable decleration(int, double, string, bool)
+
+
+int age = 50;
+
+string name = "Al Ameen";
+
+double height = 32.5;
+
+bool isPresent = false;
+
+age = 25;
+/////////////////////////////////
+
+
+// Console.WriteLine(name + " " + height ); //string concatination
+//
+// Console.WriteLine($"{name} {height}"); //string interpolation
+
+Console.WriteLine("My name is " + name + " I am " + age + " years old, " + (isPresent=false),"");
+
+Console.WriteLine($"My_name_is_{name}_and_I_am_{age}_years_old_my_height_is_{height}");
